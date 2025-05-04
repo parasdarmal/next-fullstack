610b9cd7c1a4662e60d44f09929961a544f3414d
Paras Darmal <paras.darmal@globallogic.com>
2025-05-04 18:54:56 +0530

    feat: advanced router in next-js

--- pages/user/[username]/index.js
+++ pages/user/[username]/index.js
@@ -1,12 +1,17 @@
 import { useRouter } from "next/router"
 
 function Index() {
-    const { query } = useRouter();
-
+    const router = useRouter();
 
     return (
         <div>
-            <h1>This is index page for {query.username}</h1>
+            <h1>This is index page for {router.query.username}</h1>
+            <button onClick={e => router.push(`/user/${router.query.username}/settings`)}>Go to settings page</button>
+            <button onClick={e => router.push({
+                pathname: "/user/[username]/settings",
+                query: {username: router.query.username}
+            })}>Go to settings page by another method</button>
+            <button onClick={e => router.reload()}>Go to settings page by another method</button>
         </div>
     )
 }
